76e948cd87384e14c048b70a93c3bbe599c4adab
Matthew Craven <matthew.craven.dev@outlook.com>
2023-12-08 17:20:06 +0000

    Move to firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 // Sass
-import './App.scss';
+import "./App.scss";
 
 // Redux
-import store from './store';
-import launchControlSlice from './launchControlSlice';
+import store from "./store";
+import launchControlSlice from "./launchControlSlice";
 
 // Components
-import LaunchControlXL from './LaunchControlXL';
-import EditPanel from './EditPanel';
+import LaunchControlXL from "./LaunchControlXL";
+import EditPanel from "./EditPanel";
 
 // Unpack Redux actions
 const { deselect, shiftEditingControl } = launchControlSlice.actions;
@@ -23,6 +23,15 @@ const App = () => {
   // Store ref to label input field so that it can be focused in the
   // LaunchControlXL component when a control is selected
   const [labelInputRef, setLabelInputRef] = useState(null);
+
+  const [showMovedPopup, setShowMovedPopup] = useState(false);
+
+  useEffect(() => {
+    if (window.location.host !== "launch-control-labeller.web.app") {
+      setShowMovedPopup(true);
+    }
+  }, []);
+
   return (
     <div
       className="w-100 h-100 d-flex justify-content-center align-items-center"
@@ -31,27 +40,53 @@ const App = () => {
       onKeyDown={(e) => {
         switch (e.key) {
           // Deselect all controls on enter press
-          case 'Enter':
+          case "Enter":
             store.dispatch(deselect());
             break;
           // Arrow key navigation around controls once
-          case 'ArrowRight':
-            store.dispatch(shiftEditingControl('right'));
+          case "ArrowRight":
+            store.dispatch(shiftEditingControl("right"));
             break;
-          case 'ArrowLeft':
-            store.dispatch(shiftEditingControl('left'));
+          case "ArrowLeft":
+            store.dispatch(shiftEditingControl("left"));
             break;
-          case 'ArrowUp':
-            store.dispatch(shiftEditingControl('up'));
+          case "ArrowUp":
+            store.dispatch(shiftEditingControl("up"));
             break;
-          case 'ArrowDown':
-            store.dispatch(shiftEditingControl('down'));
+          case "ArrowDown":
+            store.dispatch(shiftEditingControl("down"));
             break;
           default:
             return;
         }
       }}
     >
+      {showMovedPopup && (
+        <div
+          className="modal fade show"
+          id="myModal"
+          style={{ display: "block" }}
+        >
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content p-4">
+              <div className="modal-header">
+                <h4 className="modal-title">
+                  Launch Control Labeller has moved home
+                </h4>
+              </div>
+
+              <div className="modal-body">
+                <p>
+                  Continue using at{" "}
+                  <a href="https://launch-control-labeller.web.app">
+                    https://launch-control-labeller.web.app
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       {/* Controller diagram */}
       <LaunchControlXL labelInputRef={labelInputRef} />
       {/* Panel with load, save and label edit functionality. */}
